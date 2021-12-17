d6cac35af46806bbb6b2b9c99b6c50aa20138056
Ильназ Газимов <ilnaz.gazimov@mail.ru>
2021-12-17 10:56:51 +0500

    Добавьте файлы проекта.

---
+++ AgendaApp/AgendaApp/Models/Agenda.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace AgendaApp.Models
+{
+    public class Agenda
+    {
+        public string DayWeek { get; set; }
+        public DateTime Date { get; set; }
+        public ObservableCollection<DayWeek> DayWeeks { get; set; }
+        public string Color { get; set; }
+
+        public ObservableCollection<Agenda> MyAgenda(DateTime DdownDay, List<DayWeek> shed) 
+        {
+            shed = AddTimeShedule(shed);
+            return GetAgenda(DdownDay, shed); 
+        }
+
+
+        private ObservableCollection<Agenda> GetAgenda(DateTime DdownDay, List<DayWeek> shed)
+        {
+            return new ObservableCollection<Agenda>
+            {
+                new Agenda { DayWeek = "Понедельник", Color = "#B96CBD", Date = DdownDay,
+                    DayWeeks = new ObservableCollection<DayWeek>(shed.Take(6)) },
+
+                new Agenda { DayWeek = "Вторник", Color = "#49A24D", Date = DdownDay.AddDays(1),
+                    DayWeeks = new ObservableCollection<DayWeek>(shed.Skip(6).Take(6)) },
+
+                new Agenda { DayWeek = "Среда", Color = "#FDA838", Date = DdownDay.AddDays(2),
+                    DayWeeks = new ObservableCollection<DayWeek>(shed.Skip(12).Take(6)) },
+
+                new Agenda { DayWeek = "Четверг", Color = "#F75355", Date = DdownDay.AddDays(3),
+                    DayWeeks = new ObservableCollection<DayWeek>(shed.Skip(18).Take(7)) },
+
+                new Agenda { DayWeek = "Пятница", Color = "#00C6AE", Date = DdownDay.AddDays(4),
+                    DayWeeks = new ObservableCollection<DayWeek>(shed.Skip(25).Take(6)) },
+
+                new Agenda { DayWeek = "Суббота", Color = "#455399", Date = DdownDay.AddDays(5),
+                    DayWeeks = new ObservableCollection<DayWeek>(shed.Skip(31).Take(6)) }
+            };
+        }
+        private List<DayWeek> AddTimeShedule(List<DayWeek> shedule)
+        {
+            for(int i = 0; i<=shedule.Count-6;)
+            {
+                if (i != 18)
+                {
+                    shedule[i].Date = "\n08:30 - 10:00";
+                    shedule[i + 1].Date = "\n10:10 - 11:40";
+                    shedule[i + 2].Date = "\n12:00 - 13:30";
+                    shedule[i + 3].Date = "\n13:50 - 15:20";
+                    shedule[i + 4].Date = "\n15:30 - 17:00";
+                    shedule[i + 5].Date = "\n17:05 - 18:35";
+                    i += 6;
+                }
+                else
+                {
+                    shedule[i].Date = "\n08:30 - 10:00";
+                    shedule[i + 1].Date = "\n10:10 - 11:40";
+                    shedule[i + 2].Date = "\n12:00 - 13:30";
+                    shedule[i + 3].Date = "\n13:40 - 14:25";
+                    shedule[i + 4].Date = "\n14:45 - 16:15";
+                    shedule[i + 5].Date = "\n16:35 - 18:05";
+                    shedule[i + 6].Date = "\n18:15 - 19:45";
+                    i += 7;
+                }
+            }
+            for (int i = 0; i < shedule.Count; i++)
+                shedule[i].Day = "\n" + shedule[i].Day;
+            return shedule;
+        }
+    }
+}
---
+++ AgendaApp/AgendaApp/Pages/TeacherPage.xaml.cs
@@ -0,0 +1,191 @@
+﻿using AgendaApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AgendaApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TeacherPage : ContentPage
+    {
+        HttpClient http = new HttpClient();
+        Date dateSchedule = new Date();
+        public TeacherPage()
+        {
+            InitializeComponent();
+            GetGroupList();
+            dpDateSchedule.Date = DateSave.date.SelectedDate;
+            dpDateSchedule_DateSelected(null, null);
+            tryingNewSchedule();
+            this.BindingContext = this;
+        }
+
+        private async void dpDateSchedule_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            while (5 > 3)
+            {
+                try
+                {
+                    loading.IsVisible = true;
+                    loading.IsAnimationPlaying = true;
+                    cvSchedule.IsVisible = false;
+                    cvSchedule.ItemsSource = null;
+                    dateSchedule.GetDate(dpDateSchedule.Date);
+                    lbFirstDay.Text = dateSchedule.downDay.ToString();
+                    lbSecondDay.Text = dateSchedule.upDay.ToString();
+                    lbFirstMonth.Text = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(dateSchedule.DupDay.Month);
+                    lbSecondMonth.Text = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(dateSchedule.DdownDay.Month);
+                    var resDate = await http.GetAsync(new Uri($"http://drankmin-001-site1.htempurl.com/api/lastdance/getdate/{dpDateSchedule.Date}"));
+                    pCabinet_SelectedIndexChanged(null, null);
+                    resDate.EnsureSuccessStatusCode();
+                    cvSchedule.IsVisible = true;
+                    loading.IsAnimationPlaying = false;
+                    loading.IsVisible = false;
+                    break;
+                }
+                catch
+                {
+                    //bool resault = await DisplayAlert("Connection Failed", "Check your internet connection!", "Try again", "Cancel");
+                    //if (resault)
+                    //{
+                        continue;
+                    //}
+                    //else
+                    //    Environment.Exit(0);
+                }
+            }
+        }
+
+        private async void pCabinet_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            while (5 > 3)
+            {
+                try
+                {
+                    if (pCabinet.SelectedIndex != -1)
+                    {
+                        loading.IsVisible = true;
+                        loading.IsAnimationPlaying = true;
+                        cvSchedule.IsVisible = false;
+                        var resGroup = await http.GetAsync(new Uri($"http://drankmin-001-site1.htempurl.com/api/lastdance/getteacher/{pCabinet.SelectedItem.ToString()}"));
+                        resGroup.EnsureSuccessStatusCode();
+                        var groupShedule = resGroup.Content.ReadAsAsync<List<DayWeek>>();
+                        List<DayWeek> list = await groupShedule;
+                        Agenda agenda = new Agenda();
+                        cvSchedule.ItemsSource = agenda.MyAgenda(DateSave.date.DdownDay, list);
+                        cvSchedule.IsVisible = true;
+                        loading.IsAnimationPlaying = false;
+                        loading.IsVisible = false;
+
+                    }
+                    break;
+                }
+                catch
+                {
+                    //bool resault = await DisplayAlert("Connection Failed", "Check your internet connection!", "Try again", "Cancel");
+                    //if (resault)
+                    //{
+                        continue;
+                    //}
+                    //else
+                    //    Environment.Exit(0);
+                }
+            }
+        }
+
+        private async void GetGroupList()
+        {
+            while (5 > 3)
+            {
+                try
+                {
+                    var resGroupList = await http.GetAsync(new Uri($"http://drankmin-001-site1.htempurl.com/api/lastdance/getteachersList"));
+                    resGroupList.EnsureSuccessStatusCode();
+                    pCabinet.ItemsSource = await resGroupList.Content.ReadAsAsync<List<string>>();
+                    break;
+                }
+                catch
+                {
+                    //bool resault = await DisplayAlert("Connection Failed", "Check your internet connection!", "Try again", "Cancel");
+                    //if (resault)
+                    //{
+                        continue;
+                    //}
+                    //else
+                    //    Environment.Exit(0);
+                }
+            }
+        }
+        private async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            while (5 > 3)
+            {
+                try
+                {
+                    if (NewSheduleBt.Source.ToString() == "gg.png")
+                    {
+                        string s = "Новое расписание!!!";
+                        var resultNewWeek = await http.GetAsync(new Uri($"http://lastDance.com/api/lastdance/getnewWeek/{s}"));
+                        resultNewWeek.EnsureSuccessStatusCode();
+                        pCabinet.SelectedIndex = -1;
+                        dpDateSchedule.Date = DateTime.Today.AddDays(5);
+                        NewSheduleBt.Source = "old.png";
+                    }
+                    else
+                    {
+                        var resNew = await http.GetAsync(new Uri($"http://drankmin-001-site1.htempurl.com/api/lastdance/getdate/{DateTime.Now.ToShortDateString()}"));
+                        resNew.EnsureSuccessStatusCode();
+                        NewSheduleBt.Source = "gg.png";
+                        pCabinet.SelectedIndex = -1;
+                        dpDateSchedule.Date = DateTime.Today;
+                    }
+                    break;
+                }
+                catch
+                {
+                    //bool resault = await DisplayAlert("Connection Failed", "Check your internet connection!", "Try again", "Cancel");
+                    //if (resault)
+                    //{
+                        continue;
+                    //}
+                    //else
+                    //    Environment.Exit(0);
+                }
+            }
+        }
+        private async void tryingNewSchedule()
+        {
+            while (5 > 3)
+            {
+                try
+                {
+                    var resnew = await http.GetAsync(new Uri($"http://drankmin-001-site1.htempurl.com/api/lastdance/getnew"));
+                    resnew.EnsureSuccessStatusCode();
+                    var res = resnew.Content.ReadAsStringAsync();
+                    if (res.ToString() == "есть новое расписание")
+                    {
+                        NewSheduleBt.IsVisible = true;
+                    }
+                    break;
+                }
+                catch
+                {
+                    //bool resault = await DisplayAlert("Connection Failed", "Check your internet connection!", "Try again", "Cancel");
+                    //if (resault)
+                    //{
+                        continue;
+                    //}
+                    //else
+                    //    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
